55347203e041a5aeed00395804014a22ae80f10a
Ilko Shterev <ilko.shterev@gmail.com>
2017-10-04 22:05:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("My name is Ilko Shterev ");
+            Console.WriteLine("My email address is : ilko.shterev@gmail.com ");
+        }
+    }
+}
